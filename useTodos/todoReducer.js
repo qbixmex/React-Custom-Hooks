5c59e865f43cb6b54456e502d8c0a8498d2cb548
Daniel González Briseño <daniel.gb@globant.com>
2022-06-23 03:43:44 -0500

    Todo Reducer Added

---
+++ useTodos/todoReducer.js
@@ -0,0 +1,26 @@
+export const todoReducer = (state = [], action) => {
+  switch (action.type) {
+    case '[TODO] Add Todo':
+      return [
+        ...state,
+        action.payload
+      ];
+
+    case '[TODO] Toggle Todo':
+      return state.map(todo => {
+        if (todo.id === action.payload) {
+          return {
+            ...todo,
+            done: !todo.done
+          };
+        }
+        return todo;
+      });
+
+    case '[TODO] Remove Todo':
+      return state.filter( todo => todo.id !== action.payload );
+  
+    default:
+      return state;
+  }
+};
